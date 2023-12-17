d3084f4a1e0f7c11878a471dd9a81c25c34c0171
Carlos Hernandez <103536484+CarlosHernandezLl@users.noreply.github.com>
2023-12-17 18:06:03 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,36 @@
+function ocultar(){
+    var menu = document.getElementsByClassName('navbar');
+    for (let index = 0; index < menu.length; index++) {
+        
+        menu[index].style.display='none';
+    }
+}
+
+function mostrar(){
+    var menu = document.getElementsByClassName('navbar');
+    for (let index = 0; index < menu.length; index++) {
+        
+        menu[index].style.display='flex';
+    }
+}
+
+
+function myfunction(){
+    var menu = document.getElementsByClassName('navbar');
+    for (let index = 0; index < menu.length; index++) {
+
+        if(menu[index].style.display=='flex'){
+
+            menu[index].style.display='none';
+        }
+        else{
+            menu[index].style.display='flex';
+        }
+    }
+    
+
+    document.addEventListener('DOMContentLoaded', myfunction());
+}
+
+
+
