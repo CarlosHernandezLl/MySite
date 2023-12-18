ccca44c5bcc6d8267e8bc7b1deab294a32abe800
Carlos Hernandez <103536484+CarlosHernandezLl@users.noreply.github.com>
2023-12-17 19:05:32 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -15,22 +15,35 @@ function mostrar(){
 }
 
 
-function myfunction(){
+function myfunction() {
     var menu = document.getElementsByClassName('navbar');
-    for (let index = 0; index < menu.length; index++) {
 
-        if(menu[index].style.display=='flex'){
-
-            menu[index].style.display='none';
-        }
-        else{
-            menu[index].style.display='flex';
+    function toggleMenu() {
+        for (let index = 0; index < menu.length; index++) {
+            if (menu[index].style.display === 'flex') {
+                menu[index].style.display = 'none';
+            } else {
+                menu[index].style.display = 'flex';
+            }
         }
     }
-    
+
+    function resetMenu() {
+        for (let index = 0; index < menu.length; index++) {
+            if (window.innerWidth > 500) {
+                menu[index].style.display = 'flex';
+            } else {
+                menu[index].style.display = 'none';
+            }
+        }
+    }
+
+    window.addEventListener('resize', resetMenu);
+    toggleMenu(); // Ejecuta la función una vez al cargar la página para establecer el estado inicial
 
 }
-//document.addEventListener('DOMContentLoaded', myfunction());
+
+document.addEventListener('DOMContentLoaded', myfunction);
 
 
 
